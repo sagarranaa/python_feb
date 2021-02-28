9fe1e8a424c3c43ec3e266dd452ad4281a7c154d
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2021-02-28 18:12:11 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,14 @@
+## dictionary
+data={1:'sagar',2:'karan',3:'kumkum',4:'kunjay'}
+print(data)
+print(data[3])
+langugage=['c++','java','php','html5','python','perl','javascript','flask']
+person=['sagar','kundan','amit','sagar','sagar','amit','uma','aadeshwishkarma']
+merge=dict(zip(langugage,person))
+print(merge)
+# how do we append data in list
+merge['saki']='android_developer'
+print(merge)
+## how to delete any data form list
+del person[6]
+print(merge)
---
+++ variable1.py
@@ -0,0 +1,5 @@
+## how to find memory addresss
+num=5
+print(id(num))
+num1='sagar'
+print(id(num1))
