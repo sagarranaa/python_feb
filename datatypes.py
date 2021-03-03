82cf2ca07b0d4a0f1abe8bb7a1ad18e075e0a095
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2021-03-03 21:59:50 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+num=25
+
+print(num)
+print(type(num))
+
+## complex datatype
+b=6+5j
+print(type(b))
+
+## range function
+
+list(range (0,20))
---
+++ list1.py
@@ -0,0 +1,15 @@
+num=[21,4,78231,575,12,78641,7465,4120,7]
+
+print(num[2:])
+
+## list of list
+
+name=['sagar','aadesh','kundan',2.5,458,45]
+
+mile=[num,name]
+print(mile)
+
+## inset function
+
+name.insert(2,45)
+print(mile)
