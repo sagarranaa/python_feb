2a96bcf5fc8a9e94f4719467d0329c980397d82b
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-01-16 07:15:28 -0500

    Add files via upload

---
+++ pattern#.py
@@ -0,0 +1,8 @@
+# Hash pattern
+# i=1
+# while i<=4:
+#     print("#####")
+#     i=i+1
+
+for i in range(1,5):
+    print("#####")
---
+++ whileL.py
@@ -0,0 +1,16 @@
+# i=1
+# while i<=100:
+#     if i%3==0:
+#         print(i,end=" ")
+#     elif i%2==0:
+#         print(i,end=" ")
+#     i=i+1
+# i=1
+# while i<=100:
+#     if i%3!=0 or i%5!=0:
+#         print(i,end=" ")
+#     i=i+1
+
+# for i in range(1,100):
+#     if i%3!=0 and i%5!=0:
+#         print(i,end=" l")
