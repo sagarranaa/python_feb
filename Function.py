916b659b90080bf01b0818a9456633837bec7466
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-01-20 09:07:08 -0500

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,13 @@
+
+# def fun(): # Function define
+#     print("Hii!Sagar",end=" ")
+#     print("Very Good morning!")
+# fun() # Function calling
+
+def sum_of_two(a,b):
+    d=a*b
+    c=a+b
+    # print(c) #EXCUTE THE DASK
+    return c,d # Return the value
+result,result1=sum_of_two(5,6)
+print(result,result1)
